68019efcd7e40ce97669228b7231b198c9e539d4
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-11 21:12:14 +0200

    Feat(App): Implemented TimerAnimation to ExerciseSetStatic

--- projects/app/src/main/java/bdisfer1410/gymapp/exercise/models/sets/ExerciseSetStatic.java
+++ projects/app/src/main/java/bdisfer1410/gymapp/exercise/models/sets/ExerciseSetStatic.java
@@ -1,6 +1,14 @@
 package bdisfer1410.gymapp.exercise.models.sets;
 
+import android.animation.ValueAnimator;
+
+import androidx.annotation.NonNull;
+
+import java.util.Objects;
+
+import bdisfer1410.gymapp.R;
 import bdisfer1410.gymapp.exercise.models.movement.ExercisePose;
+import bdisfer1410.gymapp.exercise.timer.view.TimerFragment;
 
 public class ExerciseSetStatic extends ExerciseSet {
     private final ExercisePose pose;
@@ -11,4 +19,37 @@ public class ExerciseSetStatic extends ExerciseSet {
         this.pose = pose;
         this.msDuration = msDuration;
     }
+
+    //region TimerAnimation
+    @Override
+    public int onStart(@NonNull TimerFragment timer) {
+        timer.setExerciseProgressMax(msDuration);
+        timer.setExerciseNameText(name);
+        timer.setExerciseIconImage(
+                Objects.requireNonNullElse(pose.getIcon(), R.drawable.ic_exercise_default)
+        );
+        return msDuration;
+    }
+
+    @Override
+    public void onEnd(@NonNull TimerFragment timer) {
+
+    }
+
+    @Override
+    public void onUpdate(@NonNull TimerFragment timer, ValueAnimator animation) {
+        int elapsedMs = (int) animation.getAnimatedValue();
+
+        int remainingMs = msDuration - elapsedMs;
+        int remainingSeconds = (remainingMs / 1000) + 1;
+        timer.setExerciseCounterText(String.valueOf(remainingSeconds));
+
+        timer.setExerciseProgressValue(elapsedMs);
+    }
+
+    @Override
+    public int onTick(@NonNull TimerFragment timer) {
+        return 0;
+    }
+    //endregion
 }
