a12783e1b0520cfd65ca91be4ad5980643f02fe0
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-12 14:50:28 +0200

    Test(App): ExerciseMock has three sets of shorter pushups and plank

--- projects/app/src/main/java/bdisfer1410/gymapp/exercise/mock/ExerciseMock.java
+++ projects/app/src/main/java/bdisfer1410/gymapp/exercise/mock/ExerciseMock.java
@@ -10,21 +10,30 @@ import bdisfer1410.gymapp.exercise.models.sets.ExerciseSetStatic;
 import bdisfer1410.gymapp.exercise.timer.controller.TimerAnimation;
 
 public class ExerciseMock {
-    public static final List<TimerAnimation> CALISTHENICS = List.of(
-            new ExerciseSetDynamic(
-                    "Flexiones",
-                    List.of(
-                            new ExerciseTransition(new ExercisePose("Empujar para abajo", null), 1_000),
-                            new ExerciseTransition(new ExercisePose("Empujar para arriba", null), 1_000)
-                    ),
-                    10
-            ),
-            new ExerciseRest(12_000),
-            new ExerciseSetStatic(
-                    "Plancha",
-                    new ExercisePose("Empujar para arriba", null),
-                    60_000
-            )
-    );
+    public static final List<TimerAnimation> CALISTHENICS;
 
+    static {
+        ExerciseSetDynamic pushups = new ExerciseSetDynamic(
+                "Flexiones",
+                List.of(
+                        new ExerciseTransition(new ExercisePose("Empujar para abajo", null), 1_000),
+                        new ExerciseTransition(new ExercisePose("Empujar para arriba", null), 1_000)
+                ),
+                3
+        );
+
+        ExerciseSetStatic plank = new ExerciseSetStatic(
+                "Plancha",
+                new ExercisePose("Empujar para arriba", null),
+                3_000
+        );
+
+        ExerciseRest rest = new ExerciseRest(6_000);
+
+        CALISTHENICS  = List.of(
+                pushups, rest, plank, rest,
+                pushups, rest, plank, rest,
+                pushups, rest, plank
+        );
+    }
 }
