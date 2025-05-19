6e36e3eb7be5845e6efa2b59e21cc3065de863d8
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-19 20:36:09 +0200

    Feat(App): Added non-dynamic UI elements of MainActivity

--- projects/android/app/src/main/java/bdisfer1410/gymapp/activity/MainActivity.java
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/activity/MainActivity.java
@@ -1,12 +1,16 @@
 package bdisfer1410.gymapp.activity;
 
 import android.content.Intent;
+import android.graphics.Insets;
 import android.os.Bundle;
 import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import bdisfer1410.gymapp.R;
-import bdisfer1410.gymapp.exercise.mock.ExerciseMock;
 import bdisfer1410.gymapp.exercise.timer.state.TimerAnimationQueue;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,9 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars()).toPlatformInsets();
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        findViewById(R.id.button1).setOnClickListener(v -> startExerciseActivity(ExerciseMock.CALISTHENICS));
-        findViewById(R.id.button2).setOnClickListener(v -> startExerciseActivity(ExerciseMock.TIMERS));
+        // findViewById(R.id.button1).setOnClickListener(v -> startExerciseActivity(ExerciseMock.CALISTHENICS));
+        // findViewById(R.id.button2).setOnClickListener(v -> startExerciseActivity(ExerciseMock.TIMERS));
     }
     //endregion
 
