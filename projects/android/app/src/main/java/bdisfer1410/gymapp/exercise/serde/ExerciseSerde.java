2adc01905caca2ad5e0225111031102681f23855
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-21 19:53:27 +0200

    Feat(app): Added ExerciseSerde interface for serializing and deserializing Exercise instances

---
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/serde/ExerciseSerde.java
@@ -0,0 +1,35 @@
+package bdisfer1410.gymapp.exercise.serde;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+import bdisfer1410.gymapp.exercise.models.Exercise;
+import bdisfer1410.gymapp.util.Result;
+
+/**
+ * Defines a contract for serializing and deserializing {@link Exercise} instances.
+ * Implementations must handle internally where the data is stored or loaded from.
+ */
+public interface ExerciseSerde {
+    /**
+     * Serializes an {@link Exercise}. The implementing object must
+     * internally define the destination where the serialized file will be saved.
+     *
+     * @param exercise The {@link Exercise} instance to serialize.
+     * @return A {@link Result} where {@code Ok<Void>} indicates success,
+     *         and {@code Err<Integer>} contains a string resource ID from
+     *         {@link bdisfer1410.gymapp.R.string} describing the error.
+     */
+    @NonNull Result<Void, Integer> serialize(List<Exercise> exercises);
+
+    /**
+     * Deserializes an {@link Exercise}. The implementing object must
+     * internally define the source from which the exercise is loaded.
+     *
+     * @return A {@link Result} where {@code Ok<Exercise>} contains the deserialized object,
+     *         and {@code Err<Integer>} contains a string resource ID from
+     *         {@link bdisfer1410.gymapp.R.string} describing the error.
+     */
+    @NonNull Result<List<Exercise>, Integer> deserialize();
+}
