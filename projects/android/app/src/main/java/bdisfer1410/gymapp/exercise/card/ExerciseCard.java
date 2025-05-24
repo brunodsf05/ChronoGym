aacb08f2b573788f88c2676e9be79d5839ce5029
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-24 14:55:31 +0200

    Feat(App): Interface that lets any object to be displayed as an ExerciseCard

---
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/card/ExerciseCard.java
@@ -0,0 +1,45 @@
+package bdisfer1410.gymapp.exercise.card;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Interface that defines the data needed to display an exercise card in the RecyclerView.
+ */
+public interface ExerciseCard {
+
+    /**
+     * Returns the resource ID of the card icon.
+     *
+     * @return Drawable resource ID (e.g., R.drawable.icon), or null if no icon should be shown.
+     */
+    @Nullable Integer getCardIcon();
+
+    /**
+     * Returns the title or main name displayed on the card.
+     *
+     * @return Name of the exercise or element represented by the card.
+     */
+    @NonNull String getCardName();
+
+    /**
+     * Returns the associated tags or categories.
+     *
+     * @return Tags as a string, or null if the tag TextView should be hidden.
+     */
+    @Nullable String getCardTags();
+
+    /**
+     * Returns the interval string shown on the card.
+     *
+     * @return Interval (e.g., "30s", "3x10") as a string.
+     */
+    @NonNull String getCardInterval();
+
+    /**
+     * Returns additional information, if any.
+     *
+     * @return Extra text as a string, or null if the extra TextView should be hidden.
+     */
+    @Nullable String getCardExtra();
+}
