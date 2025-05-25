9a805834c9ed01200b43abef83b56801b889db4f
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-25 20:04:06 +0200

    Feat(App): SimpleCard for creating interfaces faster

---
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/activity/editor/SimpleCard.java
@@ -0,0 +1,67 @@
+package bdisfer1410.gymapp.activity.editor;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import bdisfer1410.gymapp.exercise.card.ExerciseCard;
+import bdisfer1410.gymapp.util.java.Identifiable;
+
+/**
+ * Simple card with only icon, text and subtext in order to create buttons inside a page.
+ * It comes bundled with {@link Identifiable} so it is that.
+ * The advantage of creating buttons with this, is that you can avoid fragments.
+ */
+public class SimpleCard extends Identifiable implements ExerciseCard {
+
+    @Nullable
+    protected Integer icon;
+
+    @NonNull
+    protected String text;
+
+    @Nullable
+    protected String subtext;
+
+    public SimpleCard(@NonNull String id, @Nullable Integer icon, @NonNull String text, @Nullable String subtext) {
+        this.id = id;
+        this.icon = icon;
+        this.text = text;
+        this.subtext = subtext;
+    }
+
+    public void setText(@NonNull String text) {
+        this.text = text;
+    }
+
+    //region ExerciseCard
+    @Nullable
+    @Override
+    public Integer getCardIcon() {
+        return this.icon;
+    }
+
+    @NonNull
+    @Override
+    public String getCardName() {
+        return this.text;
+    }
+
+    @Nullable
+    @Override
+    public String getCardTags() {
+        return this.subtext;
+    }
+
+    @NonNull
+    @Override
+    public String getCardInterval() {
+        return "";
+    }
+
+    @Nullable
+    @Override
+    public String getCardExtra() {
+        return "";
+    }
+    //endregion
+}
