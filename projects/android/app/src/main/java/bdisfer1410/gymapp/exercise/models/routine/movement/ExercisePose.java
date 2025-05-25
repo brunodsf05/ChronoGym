e7336c70ed9fc7986da89afed562256b30bbc816
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-25 12:52:56 +0200

    Feat(App): Implemented ExerciseCard for ExercisePose

--- projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/models/routine/movement/ExercisePose.java
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/models/routine/movement/ExercisePose.java
@@ -1,13 +1,20 @@
 package bdisfer1410.gymapp.exercise.models.routine.movement;
 
+import android.annotation.SuppressLint;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.io.Serializable;
 
+import bdisfer1410.gymapp.exercise.card.ExerciseCard;
 import bdisfer1410.gymapp.util.java.Identifiable;
+import bdisfer1410.gymapp.util.java.StringUtils;
 
 /**
  * Stores information about an static pose that's done during any exercise.
  */
-public class ExercisePose extends Identifiable implements Serializable {
+public class ExercisePose extends Identifiable implements Serializable, ExerciseCard {
     private final String name;
     private final Integer icon;
 
@@ -24,4 +31,36 @@ public class ExercisePose extends Identifiable implements Serializable {
     public Integer getIcon() {
         return icon;
     }
+
+    //region ExerciseCard
+    @Nullable
+    @Override
+    public Integer getCardIcon() {
+        return getIcon();
+    }
+
+    @NonNull
+    @Override
+    public String getCardName() {
+        return getName();
+    }
+
+    @Nullable
+    @Override
+    public String getCardTags() {
+        return getPrettierId();
+    }
+
+    @NonNull
+    @Override
+    public String getCardInterval() {
+        return "";
+    }
+
+    @Nullable
+    @Override
+    public String getCardExtra() {
+        return "";
+    }
+    //endregion
 }
