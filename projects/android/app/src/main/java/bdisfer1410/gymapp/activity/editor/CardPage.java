c6f96b7d738877d69d5f15f1c8d1e3bc2ff35ad7
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-25 11:27:32 +0200

    Feat(App): Created a CardPage storage object and Pager adapter

---
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/activity/editor/CardPage.java
@@ -0,0 +1,23 @@
+package bdisfer1410.gymapp.activity.editor;
+
+import java.util.List;
+
+import bdisfer1410.gymapp.exercise.card.ExerciseCard;
+
+public class CardPage {
+    private final String name;
+    private final List<ExerciseCard> cards;
+
+    public CardPage(String name, List<ExerciseCard> cards) {
+        this.name = name;
+        this.cards = cards;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<ExerciseCard> getCards() {
+        return cards;
+    }
+}
---
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/activity/editor/PagerEditorCardsAdapter.java
@@ -0,0 +1,87 @@
+package bdisfer1410.gymapp.activity.editor;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+import bdisfer1410.gymapp.R;
+import bdisfer1410.gymapp.exercise.card.ExerciseCard;
+import bdisfer1410.gymapp.exercise.card.ExerciseCardAdapter;
+
+public class PagerEditorCardsAdapter extends RecyclerView.Adapter<PagerEditorCardsAdapter.PageViewHolder> {
+
+    private final List<CardPage> pages;
+    private final Consumer<ExerciseCard> onClick;
+
+    public PagerEditorCardsAdapter(
+            List<CardPage> pages,
+            Consumer<ExerciseCard> onClick
+    ) {
+        this.pages = pages;
+        this.onClick = onClick;
+    }
+
+    @NonNull
+    @Override
+    public PageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new PageViewHolder(
+                LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.page_editor_card__exercise_list, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PageViewHolder holder, int position) {
+        CardPage page = pages.get(position);
+        ExerciseCardAdapter adapter = new ExerciseCardAdapter(page.getCards(), onClick, null);
+        holder.recyclerView.setLayoutManager(new LinearLayoutManager(holder.recyclerView.getContext()));
+        holder.recyclerView.setAdapter(adapter);
+
+        ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView,
+                                  @NonNull RecyclerView.ViewHolder viewHolder,
+                                  @NonNull RecyclerView.ViewHolder target) {
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                if (from < 0 || to < 0 || from == to) return false;
+                List<ExerciseCard> cards = page.getCards();
+                ExerciseCard moved = cards.remove(from);
+                cards.add(to, moved);
+                adapter.notifyItemMoved(from, to);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                // No swipe actions
+            }
+        });
+
+        helper.attachToRecyclerView(holder.recyclerView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return pages.size();
+    }
+
+    static class PageViewHolder extends RecyclerView.ViewHolder {
+        RecyclerView recyclerView;
+
+        public PageViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recyclerView = itemView.findViewById(R.id.list);
+        }
+    }
+}
