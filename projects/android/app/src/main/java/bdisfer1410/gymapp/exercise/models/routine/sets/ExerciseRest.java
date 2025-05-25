e4ff47e84a4b58fa74ca00667a0c8d62e87b5bf4
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-25 13:30:20 +0200

    Feat(App): Implemented ExerciseCard for ExerciseRest

--- projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/models/routine/sets/ExerciseRest.java
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/models/routine/sets/ExerciseRest.java
@@ -3,16 +3,21 @@ package bdisfer1410.gymapp.exercise.models.routine.sets;
 import android.animation.ValueAnimator;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import bdisfer1410.gymapp.R;
+import bdisfer1410.gymapp.exercise.card.ExerciseCard;
 import bdisfer1410.gymapp.util.java.Identifiable;
 import bdisfer1410.gymapp.exercise.timer.controller.TimerAnimation;
 import bdisfer1410.gymapp.exercise.timer.view.TimerFragment;
+import bdisfer1410.gymapp.util.java.StringUtils;
 import bdisfer1410.gymapp.util.media.Beep;
 
-public class ExerciseRest extends Identifiable implements TimerAnimation, Serializable {
+public class ExerciseRest extends Identifiable implements TimerAnimation, Serializable, ExerciseCard {
+    private final int DEFAULT_ICON = R.drawable.ic_exercise_rest;
     private final int msDuration;
     private final int msBeforeBeepStart = 10_000;
     private int msToEmitBeep;
@@ -27,7 +32,7 @@ public class ExerciseRest extends Identifiable implements TimerAnimation, Serial
     public int onStart(@NonNull TimerFragment timer) {
         timer.setExerciseNameText(R.string.model_exercise_rest);
         timer.setExerciseProgressMax(msDuration);
-        timer.setExerciseIconImage(R.drawable.ic_exercise_rest);
+        timer.setExerciseIconImage(DEFAULT_ICON);
 
         hasToEmitBeep = false;
         msToEmitBeep = (msDuration < msBeforeBeepStart)
@@ -67,6 +72,38 @@ public class ExerciseRest extends Identifiable implements TimerAnimation, Serial
     @Override
     public int calculateDuration() {
         return msDuration;
+    }
+    //endregion
+
+    //region ExerciseCard
+    @Nullable
+    @Override
+    public Integer getCardIcon() {
+        return DEFAULT_ICON;
+    }
+
+    @NonNull
+    @Override
+    public String getCardName() {
+        return "Descanso";
+    }
+
+    @Nullable
+    @Override
+    public String getCardTags() {
+        return getPrettierId();
+    }
+
+    @NonNull
+    @Override
+    public String getCardInterval() {
+        return StringUtils.formatMsIntoTime(msDuration);
+    }
+
+    @Nullable
+    @Override
+    public String getCardExtra() {
+        return "";
     }
     //endregion
 }
