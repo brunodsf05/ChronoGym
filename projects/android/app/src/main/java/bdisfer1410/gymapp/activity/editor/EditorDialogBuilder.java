137ca2a253ba2072e508f600a9e7315d56dacb07
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-05-29 18:20:45 +0200

    Feat(App): Added transition form builder

--- projects/android/app/src/main/java/bdisfer1410/gymapp/activity/editor/EditorDialogBuilder.java
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/activity/editor/EditorDialogBuilder.java
@@ -27,6 +27,35 @@ public class EditorDialogBuilder {
                 context.getString(R.string.activity_editor_dialog_pose_title),
                 "",
                 false,
+                true,
+                0,
+                defaultId,
+                defaultName,
+                defaultIconResId,
+                0,
+                blackListIds,
+                listener
+        );
+    }
+
+    public static void transitions(Context context, @NonNull List<String> blackListIds, EditorDialog.OnFormSubmittedListener listener) {
+        transitions(context, blackListIds, null, null, -1, listener);
+    }
+
+    public static void transitions(
+            Context context,
+            @NonNull List<String> blackListIds,
+            @Nullable String defaultId,
+            @Nullable String defaultName,
+            int defaultIconResId,
+            EditorDialog.OnFormSubmittedListener listener
+    ) {
+        EditorDialog.showEditorDialog(
+                context,
+                context.getString(R.string.activity_editor_dialog_transition_list_title),
+                "",
+                false,
+                false,
                 0,
                 defaultId,
                 defaultName,
