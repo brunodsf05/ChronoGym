6f85c2fad35bf9e736ec4c396332b7fbd28bbf7e
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-06-05 17:37:27 +0200

    Feat(Api): Read url params and parse it into tags lists

--- projects/python/app.py
+++ projects/python/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 import database
 import webio
@@ -11,6 +11,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
+    tags_exclusive = webio.split_line_into_words(request.args.get("exclusive", ""))
+    tags_inclusive = webio.split_line_into_words(request.args.get("inclusive", ""))
     filtered_exercises = database.exercises
     http_response = webio.serialize_json_into_string(filtered_exercises)
     return http_response
