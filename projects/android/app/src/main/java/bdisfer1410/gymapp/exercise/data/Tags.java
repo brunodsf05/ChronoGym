37ab51ab502d3e268d8fae150c0b7eda8c2e54cf
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-06-14 12:17:04 +0200

    Feat(App): Added final icons

--- projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/data/Tags.java
+++ projects/android/app/src/main/java/bdisfer1410/gymapp/exercise/data/Tags.java
@@ -21,6 +21,6 @@ public class Tags {
             Map.entry("flexibility", R.string.file_json_string_flexibility),
             Map.entry("machine", R.string.file_json_string_machine),
             Map.entry("cable", R.string.file_json_string_cable),
-            Map.entry("needs_cable_machine", R.string.file_json_string_needs_cable_machine),
+            Map.entry("needs_cable_machine", R.string.file_json_string_needs_cable_machine)
     );
 }
